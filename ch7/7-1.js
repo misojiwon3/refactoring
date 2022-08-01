4c8569295a56f91a25f88e9892bb11a130b47a69
hanseung <hslee@ecstel.co.kr>
2022-08-01 09:02:05 +0900

    clear 7-1, 7-2

--- ch7/7-1.js
+++ ch7/7-1.js
@@ -1,4 +1,22 @@
-const organization = { name: 'Acme Gooseberries', country: 'GB' };
+class Organization {
+  #name;
+  #country;
+
+  constructor(name, country) {
+    this.#name = name;
+    this.#country = country;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get country() {
+    return this.#country;
+  }
+}
+
+const organization = new Organization('Acme Gooseberries', 'GB');
 
 organization.name = 'Dream Coding';
 console.log(organization.name);
--- ch7/7-2.js
+++ ch7/7-2.js
@@ -11,11 +11,23 @@ export class Person {
   }
 
   get courses() {
+    // return [...this.#courses];
     return this.#courses;
   }
 
-  set courses(courses) {
-    this.#courses = courses;
+  addCourse(course) {
+    this.#courses.push(course);
+  }
+
+  removeCourse(course, runIfAbsent) {
+    const index = this.#courses.indexOf(course);
+
+    if (index === -1) {
+      runIfAbsent();
+      return;
+    };
+
+    this.#courses.splice(course, 1);
   }
 }
 
@@ -37,5 +49,14 @@ export class Course {
 }
 
 const ellie = new Person('엘리');
-ellie.courses.push(new Course('리팩토링', true));
+console.log(ellie.courses)
+ellie.courses.push(new Course('리팩토링', true))
+console.log(ellie.courses.length);
+
+const course = new Course('리팩토링', true);
+ellie.addCourse(course);
+console.log(ellie.courses.length);
+ellie.removeCourse(course, () => {
+  console.log('해당 코스는 없다!')
+});
 console.log(ellie.courses.length);
