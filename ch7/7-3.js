edb1ab273fa28782b66c6be1de1e8fa66ea98825
hanseung <hslee@ecstel.co.kr>
2022-08-02 23:16:05 +0900

    clear 7-3 ~ 7-9

--- ch7/7-3.js
+++ ch7/7-3.js
@@ -2,6 +2,35 @@ export class Order {
   constructor(data) {
     this.priority = data.priority;
   }
+
+  isHighPriority() {
+    return this.priority === 'high' || this.priority === 'rush';
+  }
+}
+
+class Priority {
+  #value;
+
+  constructor(value) {
+    if (Priority.legalValues().includes(value))  {
+      this.#value = value;
+    } else {
+      throw new Error(`${value} is invalid for Priority`);
+    }
+  }
+
+  get index() {
+    return Priority.legalValues().indexOf(this.#value);
+  }
+
+  equals(others) {
+    return this.#index === others.#index;
+  }
+
+
+  static legalValues() {
+    return ['low', 'normal', 'high', 'rush'];
+  }
 }
 
 const orders = [
@@ -10,6 +39,6 @@ const orders = [
   new Order({ priority: 'rush' }),
 ];
 
-const highPriorityCount = orders.filter(
-  (o) => 'high' === o.priority || 'rush' === o.priority
-).length;
+const highPriorityCount = orders.filter((o) => o.isHighPriority()).length;
+
+console.log(highPriorityCount);
